ebef6e129724dd263015bab80e691086e4423fe8
farchettiensis <farchettiensis@users.noreply.github.com>
2024-07-05 17:54:47 -0300

    Ajuste rotas

--- sef-client/src/app/pages/inicio/inicio.component.ts
+++ sef-client/src/app/pages/inicio/inicio.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
-  selector: 'sef-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.scss'
+    selector: 'sef-inicio',
+    templateUrl: './inicio.component.html',
+    styleUrl: './inicio.component.scss',
 })
 export class InicioComponent {
-
+    constructor(public router: Router, public layoutService: LayoutService) {}
 }
--- sef-client/src/app/pages/inicio/inicio.module.ts
+++ sef-client/src/app/pages/inicio/inicio.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './inicio.component';
+import { DividerModule } from 'primeng/divider';
+import { StyleClassModule } from 'primeng/styleclass';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-    imports: [CommonModule, InicioRoutingModule],
+    imports: [
+        CommonModule,
+        InicioRoutingModule,
+        DividerModule,
+        StyleClassModule,
+        ButtonModule
+    ],
     declarations: [InicioComponent],
 })
 export class InicioModule {}
